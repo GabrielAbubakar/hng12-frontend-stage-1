e4a226fa97f8a1fcecabdf8a8a44a17ff9b05a6f
GabrielAbubakar <gabrielogrima@gmail.com>
2025-02-02 23:08:29 +0100

    some setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import GameInstructions from "./components/gameInstructions"
 
 const App = () => {
     return (
-        <div className="bg-red-500">App</div>
+        <div className="p-5">
+            <h1 className="font-bold text-4xl">Color Matcher</h1>
+
+            <div>
+                <p>Color Display</p>
+                <div data-testid="colorBox"></div>
+            </div>
+
+            <div className="flex gap-2">
+                <button className="bg-red-600 px-3 text-white cursor-pointer border border-gray-600 hover:-translate-y-1 transition-all" data-testid="colorOption" >Red</button>
+                <button className="bg-blue-600 px-3 text-white cursor-pointer border border-gray-600 hover:-translate-y-1 transition-all" data-testid="colorOption" >Blue</button>
+                <button className="bg-yellow-600 px-3 text-white cursor-pointer border border-gray-600 hover:-translate-y-1 transition-all" data-testid="colorOption" >Yellow</button>
+                <button className="bg-orange-600 px-3 text-white cursor-pointer border border-gray-600 hover:-translate-y-1 transition-all" data-testid="colorOption" >Orange</button>
+                <button className="bg-green-600 px-3 text-white cursor-pointer border border-gray-600 hover:-translate-y-1 transition-all" data-testid="colorOption" >Green</button>
+                <button className="bg-purple-600 px-3 text-white cursor-pointer border border-gray-600 hover:-translate-y-1 transition-all" data-testid="colorOption" >Purple</button>
+            </div>
+
+            <GameInstructions />
+        </div>
     )
 }
 
---
+++ src/components/gameInstructions.tsx
@@ -0,0 +1,14 @@
+
+const GameInstructions = () => {
+    return (
+        <div data-testid="gameInstructions" className="bg-gray-200 p-6 absolute top-5 right-5">
+            <h1 className="font-bold mb-3">Game Instructions🎮💡</h1>
+
+            <ul>
+                <li>dsf</li>
+            </ul>
+        </div>
+    )
+}
+
+export default GameInstructions
